15b3fc09c93067963b174640720ff4c1638c7307
Oleiks <oolleekk515@wp.pl>
2024-10-21 16:57:54 +0200

    requests added back and song mapper added to musicgenres

--- src/main/java/com/example/demo/musicGenre/MusicGenreMapper.java
+++ src/main/java/com/example/demo/musicGenre/MusicGenreMapper.java
@@ -1,10 +1,12 @@
 package com.example.demo.musicGenre;
 
+import com.example.demo.song.SongMapper;
+
 public class MusicGenreMapper {
     public static MusicGenreDto toMusicGenreDto(MusicGenre musicGenre) {
         return MusicGenreDto.builder()
                 .genre(musicGenre.getGenre())
-                .songs(musicGenre.getSongs())
+                .songs(musicGenre.getSongs().stream().map(SongMapper::toSongDto).toList())
                 .id(musicGenre.getId())
                 .yearOfOrigin(musicGenre.getYearOfOrigin())
                 .build();
